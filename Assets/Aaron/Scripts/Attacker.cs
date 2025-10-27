86aced739605b29f931ced58f44820b3e478e899
Aaron Isara <aaisara1212@gmail.com>
2025-10-26 17:39:35 -0700

    Add in dummy player controllable via input system

---
+++ Assets/Aaron/Scripts/Attacker.cs
@@ -0,0 +1,70 @@
+#nullable enable
+
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Attacker : MonoBehaviour
+{
+    [SerializeField] private GameObject? attackIndicator;
+    [SerializeField] private float cooldownSeconds;
+
+    private DateTimeOffset lastAttackTime;
+    private Coroutine? activeAttackCoroutine;
+
+    private void Awake()
+    {
+        if (attackIndicator == null)
+        {
+            Debug.LogError("Attack indicator is not set");
+            return;
+        }
+        
+        attackIndicator.SetActive(false);
+    }
+    
+    private void OnAttack(InputValue value)
+    {
+        if (attackIndicator == null)
+        {
+            Debug.LogError("Attack indicator not set");
+            return;
+        }
+        
+        if (cooldownSeconds < 0)
+        {
+            Debug.LogError("Attacker cooldown seconds can't be less than 0");
+            return;
+        }
+
+        if (lastAttackTime.AddSeconds(cooldownSeconds) > DateTimeOffset.Now)
+        {
+            // Attack on cooldown
+            Debug.Log("Attack is on cooldown.");
+            return;
+        }
+        
+        attackIndicator.SetActive(true);
+
+        if (activeAttackCoroutine != null)
+        {
+            // Reset animation state in case it's not finished yet
+            StopCoroutine(activeAttackCoroutine);
+            attackIndicator.SetActive(false);
+        }
+        
+        activeAttackCoroutine = StartCoroutine(GetAttackForSecondsEnumerator(attackIndicator, cooldownSeconds));
+    }
+
+    private IEnumerator GetAttackForSecondsEnumerator(GameObject attackIndicatorRef, float seconds)
+    {
+        lastAttackTime = DateTimeOffset.UtcNow;
+        
+        attackIndicatorRef.SetActive(true);
+        
+        yield return new WaitForSeconds(seconds);
+        
+        attackIndicatorRef.SetActive(false);
+    }
+}
---
+++ Assets/Aaron/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+#nullable enable
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private Vector2 currentMoveDireciton;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(new Vector2(currentMoveDireciton.x, currentMoveDireciton.y) * (moveSpeed * Time.deltaTime));
+    }
+
+
+    private void OnMove(InputValue value)
+    {
+        currentMoveDireciton = value.Get<Vector2>().normalized;
+    }
+}
