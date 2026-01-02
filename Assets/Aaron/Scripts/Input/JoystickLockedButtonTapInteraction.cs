c0cc7d6cd631f9f7b0c42cc01225a7eddf825351
Aaron Isara <aaisara1212@gmail.com>
2026-01-01 21:33:11 -0800

    Fix attack firing on joystick flicks (which are meant for sword throws) by enforcing stable joystick during attack action

---
+++ Assets/Aaron/Scripts/Input/JoystickLockedButtonTapInteraction.cs
@@ -0,0 +1,75 @@
+#nullable enable
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Represents an interaction where a button tap is only registered if the joystick is held steady, within some threshold.
+/// </summary>
+public class JoystickLockedButtonTapInteraction : IInputInteraction<Vector3>
+{
+    public float joystickSafeZone = 0.2f;
+    public float secondsBeforeTapInvalidated = 0.3f;
+
+    private bool isDisabled = false;
+    
+    public void Process(ref InputInteractionContext context)
+    {
+        var input = context.ReadValue<Vector3>();
+        
+        var joystickInput = new Vector2(input.x, input.y);
+        bool isButtonPressed = input.z > 0.5f;
+        
+        float magnitude = joystickInput.magnitude;
+        bool isInSafeZone = magnitude <= joystickSafeZone;
+
+        switch (context.phase)
+        {
+            case InputActionPhase.Waiting:
+            {
+                if (isDisabled)
+                {
+                    if (isButtonPressed == false)
+                    {
+                        isDisabled = false;
+                    }
+
+                    break;
+                }
+
+                if (isInSafeZone == false)
+                {
+                    break;
+                }
+
+                if (isButtonPressed)
+                {
+                    context.Started();
+                    context.SetTimeout(secondsBeforeTapInvalidated);
+                }
+
+                break;
+            }
+            case InputActionPhase.Started:
+            {
+                if (context.timerHasExpired || isInSafeZone == false)
+                {
+                    isDisabled = true;
+                    context.Canceled();
+                    break;
+                }
+
+                if (isButtonPressed == false)
+                {
+                    context.Performed();
+                }
+
+                break;
+            }
+        }
+    }
+        
+    public void Reset()
+    {
+    }
+}
