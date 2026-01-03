c4731a4ba2243c97f5065dc9b0be986426f9c39c
Aaron Isara <aaisara1212@gmail.com>
2026-01-02 16:11:43 -0800

    Retrofitted PlayerController to use new gameplay input manager + updated scenes to accommodate this change

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,12 @@
 #nullable enable
 
 using AYellowpaper.SerializedCollections;
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : PlayerGameplayPawn
 {
     [SerializeField] private UnityEvent onDeath = new UnityEvent();
 
@@ -39,13 +37,12 @@ public class PlayerController : MonoBehaviour
     }
 
     PlayerState playerState = PlayerState.MeleeReady;
-    IMeleeWeapon? curWeapon;
     private Rigidbody2D? rb;
 
     [Header("Sword Recall")]
     [SerializeField] ParticleSystem? recallParticles;
     [SerializeField] float recallTime = 1f;
-    float curRecallTimer = 0f;
+    private Coroutine? recallSwordCoroutine;
 
     private void Awake()
     {
@@ -63,25 +60,7 @@ public class PlayerController : MonoBehaviour
         }
         currentHp = maxHp;
     }
-
-    private void Start()
-    {
-        if (GameManager.Instance != null)
-            curWeapon = elementToWeapon[GameManager.Instance.currentElement];
-        InputManager.OnReleaseInIdleZone += OnReleaseInIdle;
-        InputManager.OnReleaseInMoveZone += OnReleaseInMove;
-        InputManager.OnPressInIdleZone += OnTapInIdle;
-        InputManager.OnDragInIdleZone += OnHoldInIdle;
-    }
-
-    private void OnDestroy()
-    {
-        InputManager.OnReleaseInIdleZone -= OnReleaseInIdle;
-        InputManager.OnReleaseInMoveZone -= OnReleaseInMove;
-        InputManager.OnPressInIdleZone -= OnTapInIdle;
-        InputManager.OnDragInIdleZone -= OnHoldInIdle;
-    }
-
+    
     public void TakeDamage(float damage)
     {
         if (currentHp <= 0) return; // Player is already dead
@@ -103,67 +82,11 @@ public class PlayerController : MonoBehaviour
         onDeath.Invoke();
     }
 
-    public void SetElement(Element element)
-    {
-        curWeapon = elementToWeapon[element];
-    }
-
-    void MeleeCharge()
-    {
-        curWeapon.ThrowIfNull(nameof(curWeapon));
-        curWeapon.Charge(transform);
-    }
-
-    void MeleeCancelCharge()
-    {
-        curWeapon.ThrowIfNull(nameof(curWeapon));
-        curWeapon.Charge(transform, true);
-    }
-
-    void MeleeAttack()
-    {
-        ElementManager.Instance.MeleeStrike(transform);
-
-        rb.ThrowIfNull(nameof(rb));
-        curWeapon.ThrowIfNull(nameof(curWeapon));
-
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        GameObject? nearestEnemy = null;
-        float shortestDistance = Mathf.Infinity;
-
-        foreach (GameObject enemy in enemies)
-        {
-            float distance = Vector2.Distance(transform.position, enemy.transform.position);
-            if (distance < shortestDistance && distance <= attackRadius)
-            {
-                shortestDistance = distance;
-                nearestEnemy = enemy;
-            }
-        }
-
-        if (nearestEnemy == null)
-        {
-            curWeapon.Strike(transform);
-            return;
-        }
-
-        Vector2 direction = (nearestEnemy.transform.position - transform.position).normalized;
-        transform.up = direction;
-
-        Vector2 dashPosition = (Vector2)transform.position + direction * (shortestDistance * dashFactor);
-        transform.position = dashPosition;
-        curWeapon.Strike(transform);
-    }
-
     void SwordThrow(Vector2 direction)
     {
-        if (playerState == PlayerState.MeleeReady)
-        {
-            //MeleeCancelCharge();
-            ElementManager.Instance.MeleeCharge(transform, true);
-            SwordProjectile.Instance.StartFlight(transform.position, direction * projectileSpeed);
-            playerState = PlayerState.SwordThrown;
-        }
+        ElementManager.Instance.MeleeCharge(transform, true);
+        SwordProjectile.Instance.StartFlight(transform.position, direction * projectileSpeed);
+        playerState = PlayerState.SwordThrown;
     }
 
     void RecallSword()
@@ -179,27 +102,23 @@ public class PlayerController : MonoBehaviour
         SwordProjectile.Instance.StopFlight();
         playerState = PlayerState.MeleeReady;
     }
-
-    private void OnMove(InputValue value)
+    
+    private IEnumerator RecallSwordAfterDelayCoroutine(float delaySecs)
     {
-        rb.ThrowIfNull(nameof(rb));
-        Vector2 v = value.Get<Vector2>();
-
-        if (v.sqrMagnitude > 0.001f)
-        {
-            float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg - 90f;
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        }
-        rb.linearVelocity = v * speed;
+        // RETROFIT: From OnHoldInIdle
+        
+        yield return new WaitForSeconds(delaySecs);
+        
+        RecallSword();
     }
 
-    private void OnReleaseInIdle(Vector2 val)
+    
+    // PlayerGameplayPawn
+    
+    public override void Attack()
     {
-        recallParticles.ThrowIfNull(nameof(recallParticles));
-        if (playerState == PlayerState.SwordThrown)
-        {
-            recallParticles.Stop();
-        }
+        // RETROFIT: From OnReleaseInIdle
+        
         if (playerState == PlayerState.MeleeReady)
         {
             //MeleeAttack();
@@ -213,23 +132,15 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnReleaseInMove(Vector2 val)
+    public override void BeginChargeAttack()
     {
+        // RETROFIT: From OnTapInIdle
+        
         recallParticles.ThrowIfNull(nameof(recallParticles));
         if (playerState == PlayerState.SwordThrown)
         {
-            recallParticles.Stop();
-        }
-        SwordThrow(val.normalized);
-    }
-
-    private void OnTapInIdle(Vector2 val)
-    {
-        recallParticles.ThrowIfNull(nameof(recallParticles));
-        if (playerState == PlayerState.SwordThrown)
-        {
-            curRecallTimer = 0f;
             recallParticles.Play();
+            recallSwordCoroutine = StartCoroutine(RecallSwordAfterDelayCoroutine(recallTime));
         }
         else if (playerState == PlayerState.MeleeReady)
         {
@@ -238,15 +149,54 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnHoldInIdle(Vector2 val)
+    public override void ReleaseChargeAttack()
     {
-        if (playerState == PlayerState.SwordThrown)
+        recallParticles?.Stop();
+        
+        if (recallSwordCoroutine != null)
         {
-            curRecallTimer += Time.deltaTime;
-            if (curRecallTimer >= recallTime)
-            {
-                RecallSword();
-            }
+            StopCoroutine(recallSwordCoroutine);
+            recallSwordCoroutine = null;
         }
+    }
+
+    public override void CancelChargeAttack()
+    {
+        recallParticles?.Stop();
+
+        if (recallSwordCoroutine != null)
+        {
+            StopCoroutine(recallSwordCoroutine);
+            recallSwordCoroutine = null;
+        }
+    }
+
+    public override void AimInDirection(Vector2 direction)
+    {
+        // RETROFIT: No corresponding functionality from old code
+    }
+
+    public override void DoAimedAttackInDirection(Vector2 direction)
+    {
+        // RETROFIT: From OnReleaseInMove
+
+        if (playerState == PlayerState.MeleeReady)
+        {
+            SwordThrow(direction.normalized);
+        }
+    }
+
+    public override void MoveInDirection(Vector2 direction)
+    {
+        // RETROFIT: From OnMove
+        
+        rb.ThrowIfNull(nameof(rb));
+
+        if (direction.sqrMagnitude > 0.001f)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+        rb.linearVelocity = direction * speed;
     }
 }
