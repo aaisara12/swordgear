90bf8a4032ca8ad994f429f79d2a001a2c3135de
Aaron Isara <aaisara1212@gmail.com>
2026-01-07 20:12:41 -0800

    Show 3 random augments instead of just 1

--- Assets/Aaron/Scripts/InGameAugmentsManager.cs
+++ Assets/Aaron/Scripts/InGameAugmentsManager.cs
@@ -94,7 +94,7 @@ public class InGameAugmentsManager : InitializeableUnrestrictedGameComponent
         }
 
         // TODO: aisara => Add logic to clear and set up new augments based on game state, augments player already has, etc.
-        var mysteryItems = augmentsCatalog.GetAnItem();
+        var mysteryItems = augmentsCatalog.GetRandomItems(3);
 
         var storeStock = new Dictionary<string, int>();
         foreach(IStoreItem mysteryItem in mysteryItems)
