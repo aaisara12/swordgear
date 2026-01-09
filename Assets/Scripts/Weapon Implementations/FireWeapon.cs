d36899257a869b9824d6fa729fb2db263c1070ce
Ming <51458829+Do3naut@users.noreply.github.com>
2026-01-09 00:59:34 -0800

    Tweak fire screen shake

--- Assets/Scripts/Weapon Implementations/FireWeapon.cs
+++ Assets/Scripts/Weapon Implementations/FireWeapon.cs
@@ -193,6 +193,7 @@ public class FireWeapon : MonoBehaviour, IElementalWeapon
 
     public void OnRangedHit(Transform player, SwordProjectile sword, Transform hitSource, EnemyController enemy, HashSet<UpgradeType> upgrades)
     {
+        Testing.CinemachineTrackingTargetFromGameManagerSetter.Shake();
         enemy.TakeDamage(GameManager.Instance.CalculateDamage(enemy.element, Element.Fire, GameManager.Instance.currentDamage * GameManager.Instance.rangedMultiplier));
         if (upgrades.Contains(UpgradeType.Fire_RangedBurn))
         {
