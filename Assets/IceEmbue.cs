e3c7a76f2794e954a49b6075f939fd828fef0ec7
Ming <51458829+Do3naut@users.noreply.github.com>
2026-07-14 23:02:59 -0700

    Tune imbue durations

--- Assets/IceEmbue.cs
+++ Assets/IceEmbue.cs
@@ -5,7 +5,7 @@ public class IceEmbue : Embue
 
     private void Start()
     {
-        damageMultiplier = 1.2f;
-        effectDuration = 30f;
+        // damageMultiplier = 1.2f;
+        // effectDuration = 30f;
     }
 }
